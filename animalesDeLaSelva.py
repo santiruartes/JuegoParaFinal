ad2b5bef09f758c79753578d7fff7d7cd537fe09
santiruartes <ruartes19@gmail.com>
2024-11-21 16:25:42 -0300

    changes in answers

--- animalesDeLaSelva.py
+++ animalesDeLaSelva.py
@@ -2,13 +2,13 @@
 
 def juego_preguntas():
     preguntas = [
-        {"pregunta": "¿Cuál es el animal más rápido de la selva?", "respuesta": "guepardo"},
+        {"pregunta": "¿Cuál es el animal más rápido de la selva?", "respuesta": "guepardo", "respuesta": "chita"},
         {"pregunta": "¿Cuál es el mamífero más grande de la selva?", "respuesta": "elefante"},
-        {"pregunta": "¿Qué animal es conocido como el 'rey de la selva'?", "respuesta": "león"},
+        {"pregunta": "¿Qué animal es conocido como el 'rey de la selva'?", "respuesta": "león", "respuesta":"leon", "respuesta":"leon"},
         {"pregunta": "¿Qué animal se cuelga de los árboles con su cola?", "respuesta": "mono"},
         {"pregunta": "¿Qué animal tiene una lengua muy larga para comer hormigas?", "respuesta": "oso hormiguero"},
-        {"pregunta": "¿Qué animal es conocido por su habilidad de cambiar de color?", "respuesta": "camaleón"},
-        {"pregunta": "¿Qué animal es conocido por su habilidad de nadar y atacar en el agua?", "respuesta": "cocodrilo"},
+        {"pregunta": "¿Qué animal es conocido por su habilidad de cambiar de color?", "respuesta": "camaleón", "respuesta": "cameleon"},
+        {"pregunta": "¿Qué animal es conocido por su habilidad de nadar y atacar en el agua?", "respuesta": "cocodrilo", "respuesta": "coco"},
     ]
     
     puntaje = 0
@@ -19,18 +19,18 @@ def juego_preguntas():
     for pregunta in preguntas:
         respuesta_usuario = input(pregunta["pregunta"] + " ").strip().lower()
         if respuesta_usuario == pregunta["respuesta"]:
-            print("¡Correcto!")
+            print("Correcto!")
             puntaje += 1
         else:
             print(f"Incorrecto. La respuesta correcta era: {pregunta['respuesta']}.")
     
     print(f"\nJuego terminado. Obtuviste {puntaje} de {len(preguntas)} puntos.")
     if puntaje == len(preguntas):
-        print("¡Excelente, conoces muy bien los animales de la selva!")
+        print("Excelente, conoces muy bien a los animales de la selva")
     elif puntaje > len(preguntas) // 2:
-        print("¡Muy bien! Tienes buenos conocimientos sobre los animales de la selva.")
+        print("Muy bien, tenes buenos conocimientos sobre los animales de la selva")
     else:
-        print("Puedes mejorar. ¡Inténtalo de nuevo para aprender más!")
+        print("Esta bien, aunque podes mejorar")
 
 # Iniciar el juego
 if __name__ == "__main__":
